59454b1e0a30ff7fb5ea66542c828a8d7cefae7a
yakisoba-bun <sirinamos98@gmail.com>
2025-11-30 17:43:17 +0000

    Add addition function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,18 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 })
 
-function runGame() {
+function runGame(gameType) {
 
+    // Generates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -34,8 +44,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
