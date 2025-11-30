6a1efa88f31e4475785a924a0b5e9043c9223528
yakisoba-bun <sirinamos98@gmail.com>
2025-11-30 17:47:24 +0000

    Add initial function for correct answer calculation

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,9 @@ function checkAnswer() {
 
 function calculateCorrectAnswer() {
 
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText);
 }
 
 function incrementScore() {
