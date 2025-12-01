2decaef6d9ed61dfdb6309ff10fbf0cf94d8aef2
yakisoba-bun <sirinamos98@gmail.com>
2025-12-01 06:01:37 +0000

    Tidy up code for deployment

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,21 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     })
   }
+
+  document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        checkAnswer();
+    }
+  })
+
+  runGame("addition");
+
 })
 
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Generates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
