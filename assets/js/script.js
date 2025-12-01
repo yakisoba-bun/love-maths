a0c2fbfc693e8c55230a4d90794ba08f6e7dec0e
yakisoba-bun <sirinamos98@gmail.com>
2025-12-01 00:47:02 +0000

    Add functions for multiply and subtract

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,9 +81,15 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion() {
-
+    
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion() {
-    
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
